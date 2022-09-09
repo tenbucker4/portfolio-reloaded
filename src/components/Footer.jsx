a00246242795f33e079ecc13e1c669e380a18b0e
Ben Tucker <bwtucker4@gmail.com>
2022-09-09 13:21:10 -0700

    Fix height of title page to center text properly

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,14 +9,14 @@ const Footer = ( { darkMode }) => {
     <footer style={{ backgroundColor: darkMode? "#1b1b1b" : "rgb(233, 233, 233)", color: darkMode? "white" : "black" }}>
         <div className="copyright">@Ben Tucker 2022</div>
         <div className="footer-links">
-            <a href="https://github.com/tenbucker4" target="_blank" rel='noreferrer'>
+            <a href="https://www.linkedin.com/in/ben-tucker-6a520911a/" target="_blank" rel='noreferrer'>
                 <Icon path={mdiLinkedin}
                 className="footer-icon"
                 title="LinkedIn Profile"
                 color={darkMode ? ("white") : ("black")}
                 />
             </a>
-            <a href="https://www.linkedin.com/in/ben-tucker-6a520911a/" target="_blank" rel='noreferrer'>
+            <a href="https://github.com/tenbucker4" target="_blank" rel='noreferrer'>
                 <Icon path={mdiGithub}
                 className="footer-icon"
                 title="GitHub Profile"
